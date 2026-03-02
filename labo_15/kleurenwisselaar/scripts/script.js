ce53c9a65ad12414449da7b7c1609fe4638cf1b2
IntiVanparijs <inti.vanparijs@student.vives.be>
2026-03-02 22:46:36 +0100

    labo 15
    labo 15

---
+++ labo_15/kleurenwisselaar/scripts/script.js
@@ -0,0 +1,7 @@
+const setup = () => {
+    [...document.getElementsByTagName("button")].forEach(button => {
+        button.addEventListener("click", () => button.classList.toggle("pressed"));
+    });
+};
+
+window.addEventListener("load", setup);
